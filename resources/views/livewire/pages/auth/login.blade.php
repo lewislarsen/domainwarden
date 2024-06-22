0020274f8605dc3e7918d260ec6d8946797b1a3c
Lewis Larsen <lewis@larsens.dev>
2024-06-22 23:07:14 +0100

    wip

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -29,7 +29,7 @@ new #[Layout('layouts.guest')] class extends Component {
 
         Session::regenerate();
 
-        $this->redirectIntended(default: route('dashboard', absolute: false), navigate: true);
+        $this->redirectIntended(default: route('domains.index', absolute: false), navigate: true);
     }
 }; ?>
 
